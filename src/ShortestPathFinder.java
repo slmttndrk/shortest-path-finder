9c2a279d956b310f935f2e077a0e8113c039ebcd
Selamettin Dirik <selamettinn.95@gmail.com>
2023-12-30 12:38:08 +0100

    Drafted project outline.

--- src/ShortestPathFinder.java
+++ src/ShortestPathFinder.java
@@ -1,5 +1,16 @@
 public class ShortestPathFinder {
     public static void main(String[] args) {
         System.out.println("Welcome to find shortest path between Dutch cities.");
+
+        //TODO Create Nodes for Graph
+        // - Name : String
+        // - Distance : int
+        // - Neighbours : Map<String, Integer>
+
+        //TODO Create the Graph with cities and their relative distances
+
+        //TODO Implement Dijkstra's shortest path algorithm
+
+        //TODO Add welcome text to navigate users
     }
 }
